369003ae54282be4f58b3435fd8f9d75a3ba3a38
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2018-07-25 00:40:13 +0200

    Dodata ruta PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require("lodash");
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require("mongodb");
 
 var {mongoose} = require('./db/mongoose');
@@ -78,6 +79,34 @@ app.delete("/todos/:id", (req, res)=>{
     res.status(400).send(e);
   });
 });
+
+app.patch("/todos/:id",(req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text","completed"]); // funkcija pick bukvalno 'izvadi svojstva' iz nekog objekta i ubaci ih kao svojstva promenljive koju definisemo (body)
+
+  if(!ObjectID.isValid(id)){
+    return res.status(406).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt =  new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.status(200).send({ todo });
+    
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
