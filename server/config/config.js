b68090de69fc7de4cf8e4fdf31b8379ba1664f38
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2018-08-02 21:05:09 +0200

    Dodato testiranje DELETE /users/me/token

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,5 @@
-var env = process.env.NODE_ENV || 'development';
+// var env = process.env.NODE_ENV || 'development';
+var env =  'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
@@ -7,3 +8,6 @@ if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
+console.log("Nacin rada.",env);
+console.log("Port na kome se radi.",process.env.PORT);
+console.log("MONGODB URI.",process.env.MONGODB_URI);
